537210c3d304b15144c3171109dd45e4967e52a9
BIRSAN RADU IOSIF <radu_birsan@yahoo.com>
2022-08-03 08:25:20 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="AS3 website coming soon" />
+        <Header title="AS3" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
